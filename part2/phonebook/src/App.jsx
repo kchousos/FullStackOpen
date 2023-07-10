483631b5e36f414adb6f8f73a569447172578b4e
Kchou <K.chousos@protonmail.com>
2023-07-11 00:01:16 +0300

    2.6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+
+const Person = ({ name }) => <li>{ name }</li>;
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]);
+  const [newName, setNewName] = useState('');
+
+  const addPerson = (e) => {
+    e.preventDefault();
+    const newPerson = { name: newName };
+    setPersons(persons.concat(newPerson));
+    setNewName('');
+  };
+
+  const handlePersonChange = (e) => {
+    setNewName(e.target.value);
+  };
+
+  return (
+    <>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input
+                  value={newName}
+                onChange={handlePersonChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+
+      <ul>
+        {persons.map(p => <Person key={p.name} name={p.name}/>)}
+      </ul>
+    </>
+  );
+};
+
+export default App;

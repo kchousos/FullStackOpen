8f745cfed72bbcc187c13ca87dd6914cfbed9c2b
Kchou <K.chousos@protonmail.com>
2023-07-10 18:33:03 +0300

    1.6

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,44 @@
+import { createElement, useState } from 'react';
+
+const Display = props => <div>{props.value}</div>;
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+);
+
+const Statistics = ({good, neutral, bad}) => {
+  return (
+    <>
+      <h1>Statistics</h1>
+      <p>Good {good}
+        <br/>
+        Neutral {neutral}
+        <br/>
+        Bad {bad}</p>
+    </>
+  );
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <>
+      <h1>Give Feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="Good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="Bad" />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}/>
+    </>
+  );
+};
+
+export default App;

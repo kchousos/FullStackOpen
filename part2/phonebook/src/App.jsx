2b70742468942a83df0bdf18d59ee07a207eda8b
Kchou <K.chousos@protonmail.com>
2023-07-11 14:49:30 +0300

    2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,24 +1,30 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [newSearch, setNewSearch] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled');
+        setPersons(response.data);
+      });
+  }, []);
 
   const addPerson = (e) => {
     e.preventDefault();
     const newPerson = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      id: persons.length + 1
     };
     if (!persons.filter(p => p.name === newName).length)
       setPersons(persons.concat(newPerson));
@@ -40,8 +46,8 @@ const App = () => {
     setNewSearch(e.target.value);
   };
   const displayPerson = newSearch
-  ? persons.filter(person => person.name.toLowerCase().search(newSearch.toLowerCase()) !== -1)
-  : persons;
+        ? persons.filter(person => person.name.toLowerCase().search(newSearch.toLowerCase()) !== -1)
+        : persons;
 
   return (
     <>

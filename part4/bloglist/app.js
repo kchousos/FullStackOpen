9ecac919b32cddcb315e668a69fe0c69a09bff99
Kchou <K.chousos@protonmail.com>
2023-07-14 17:52:26 +0300

    4.2

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,8 +1,8 @@
-const config = require('./utils/config')
+const cors = require('cors')
 const mongoose = require('mongoose')
 const express = require('express')
-const cors = require('cors')
-const Blog = require('./models/blog')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
 
 const app = express()
 
@@ -10,23 +10,6 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,6 +1,7 @@
 const config = require('./utils/config')
 const app = require('./app')
+const logger = require('./utils/logger')
 
 app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
+  logger.info(`Server running on port ${config.PORT}`)
 })

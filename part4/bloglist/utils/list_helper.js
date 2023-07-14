6c48a4d1d8e615e64af8820e4dd0b1714ba4a457
Kchou <K.chousos@protonmail.com>
2023-07-14 18:55:45 +0300

    4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,5 +1,10 @@
-const dummy = (blogs) => {
-  return 1
+const dummy = blogs => 1
+
+const totalLikes = blogs => {
+  const red = (sum, blog) => sum + blog.likes
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce(red, 0)
 }
 
-module.exports = { dummy }
+module.exports = { dummy, totalLikes }

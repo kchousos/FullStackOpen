b5fb974ce46ee5528e65e8f449a0d01efb89f4db
Kchou <K.chousos@protonmail.com>
2023-07-22 17:03:28 +0300

    update

--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+      ? process.env.TEST_MONGODB_URI
+      : process.env.MONGODB_URI
 const PORT = 3003
 
 module.exports = { MONGODB_URI, PORT }

2c5d951d42bdc6d56e450e8627c7b983785f0090
KChousos <k.chousos@protonmail.com>
2023-07-28 19:48:44 +0300

    4.8

---
+++ part4/bloglist/tests/blogs_api.test.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  let BlogObject = new Blog(helper.initialBlogs[0])
+  await BlogObject.save()
+
+  BlogObject = new Blog(helper.initialBlogs[1])
+  await BlogObject.save()
+
+  BlogObject = new Blog(helper.initialBlogs[2])
+  await BlogObject.save()
+
+  BlogObject = new Blog(helper.initialBlogs[4])
+  await BlogObject.save()
+
+  BlogObject = new Blog(helper.initialBlogs[5])
+  await BlogObject.save()
+
+  BlogObject = new Blog(helper.initialBlogs[6])
+  await BlogObject.save()
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -14,8 +14,8 @@ const blog = [
     author: 'Edsger W. Dijkstra',
     url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
     likes: 5,
-    __v: 0
-  }
+    __v: 0,
+  },
 ]
 
 const blogs = [
@@ -25,7 +25,7 @@ const blogs = [
     author: 'Edsger W. Dijkstra',
     url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
     likes: 5,
-    __v: 0
+    __v: 0,
   },
   {
     _id: '5a422a851b54a676234d17f7',
@@ -33,7 +33,7 @@ const blogs = [
     author: 'Michael Chan',
     url: 'https://reactpatterns.com/',
     likes: 7,
-    __v: 0
+    __v: 0,
   },
   {
     _id: '5a422b3a1b54a676234d17f9',
@@ -41,7 +41,7 @@ const blogs = [
     author: 'Edsger W. Dijkstra',
     url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
     likes: 12,
-    __v: 0
+    __v: 0,
   },
   {
     _id: '5a422b891b54a676234d17fa',
@@ -49,7 +49,7 @@ const blogs = [
     author: 'Robert C. Martin',
     url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
     likes: 10,
-    __v: 0
+    __v: 0,
   },
   {
     _id: '5a422ba71b54a676234d17fb',
@@ -57,7 +57,7 @@ const blogs = [
     author: 'Robert C. Martin',
     url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
     likes: 0,
-    __v: 0
+    __v: 0,
   },
   {
     _id: '5a422bc61b54a676234d17fc',
@@ -65,8 +65,8 @@ const blogs = [
     author: 'Robert C. Martin',
     url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
     likes: 2,
-    __v: 0
-  }
+    __v: 0,
+  },
 ]
 
 describe('total likes', () => {
@@ -107,7 +107,7 @@ describe('most likes', () => {
   test('per author', () => {
     const result = listHelper.mostLikes(blogs)
     expect(result).toEqual({
-      author: "Edsger W. Dijkstra",
+      author: 'Edsger W. Dijkstra',
       likes: 17,
     })
   })

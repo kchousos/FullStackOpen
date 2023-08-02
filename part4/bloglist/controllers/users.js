d402bfa9babd0f10b54d37ea575a10c3e5c219e9
KChousos <k.chousos@protonmail.com>
2023-08-02 13:31:57 +0300

    fixed tests

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -5,18 +5,22 @@ const User = require('../models/user')
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(password, saltRounds)
+  try {
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
 
-  const user = new User({
-    username,
-    name,
-    passwordHash,
-  })
-
-  const savedUser = await user.save()
-
-  response.status(201).json(savedUser)
+    const user = new User({
+      username,
+      name,
+      passwordHash,
+    })
+    const savedUser = await user.save()
+    response.status(201).json(savedUser)
+  } catch (err) {
+    response.status(400).json({
+      error: err.message,
+    })
+  }
 })
 
 usersRouter.get('/', async (request, response) => {

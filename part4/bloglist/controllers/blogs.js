7142d47299ee88a9df8731e1d4cff470b21d5466
KChousos <k.chousos@protonmail.com>
2023-08-02 14:15:02 +0300

    4.17

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,18 +1,27 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const { title, url } = request.body
-  if (!title || !url) return response.status(400).end()
+  const body = request.body
+  const user = await User.findById(body.userId)
 
-  const blog = new Blog(request.body)
+  if (!body.title || !body.url) return response.status(400).end()
+
+  const blog = new Blog({
+    title: body.title,
+    url: body.url,
+    user: user.id,
+  })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
   response.status(201).json(savedBlog)
 })
 

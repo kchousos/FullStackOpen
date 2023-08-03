56ba5774a8a2b7c81502c711ac8bf57cc465b6ec
KChousos <k.chousos@protonmail.com>
2023-08-03 18:17:29 +0300

    5.1

---
+++ part5/bloglist-frontend/src/App.js
@@ -0,0 +1,72 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('') 
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({ username, password })
+
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      // setErrorMessage('Wrong credentials')
+      // setTimeout(() => { setErrorMessage(null) }, 5000)
+    }
+  }
+
+  if (user === null) {
+    return (
+      <>
+        <h2>Log in to application</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+            <input
+              type="text"
+              value={username}
+              name="Username"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            password
+            <input
+              type="password"
+              value={password}
+              name="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">login</button>
+        </form>
+      </>
+    )
+  }
+
+  return (
+    <>
+      <p>{user.name} logged in</p>
+      <h2>blogs</h2>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </>
+  )
+}
+
+export default App

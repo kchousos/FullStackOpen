fe1c94c7edebfdca74211fbd88d06ffbdfeef2b0
KChousos <k.chousos@protonmail.com>
2023-08-17 20:33:25 +0300

    started 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,41 @@
-const Blog = ({blog}) => (
-  <div className='blog'>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [info, setInfo] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 5,
+    paddingLeft: 2,
+    // border: 'solid',
+    // borderWidth: 1,
+    marginBottom: 5
+  }
+
+  if (info === false) {
+    return (
+      <div style={blogStyle}>
+        <div className='blog'>
+          {blog.title} {blog.author}
+          <button type="submit">view</button>
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div className='blog'>
+        {blog.title}
+        <br/>
+        {blog.url}
+        <br/>
+        likes {blog.likes}
+        <br/>
+        {blog.author}
+      </div>
+      <button type="submit">hide</button>
+    </div>
+  )
+}
 
 export default Blog
